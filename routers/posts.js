c650c268031ac3f57e50bc95eb0c9bc99909ed0a
TOMMASOMEROLA <MEROLATOMMASO77@GMAIL.COM>
2025-02-06 18:30:40 +0100

    fai rotte crud

--- routers/posts.js
+++ routers/posts.js
@@ -3,5 +3,30 @@ const router = express.Router();
 // importiamo l array di posts
 const post = require("../data/data_posts")
 
+// index - lista dei post
+router.get("/", (req, res)=> {
+    res.send("lista dei post");
+});
+
+// show - mostra un singolo post
+router.get("/:id", (req, res)=> {
+    res.send(`Mostra il post con ID: ${req.params.id}`);
+});
+
+// create - crea nuovo post
+router.post("/", (req, res) => {
+    res.send("creazione nuovo post")
+})
+
+// update - aggiorna un post esistente
+router.put("/:id", (req, res) =>{
+    res.send (`Aggiornamento del post con ID: ${req.params.id}`);
+})
+
+// delete - Cancella un post
+router.delete("/:id", (req, res) => {
+    res.send(`Cancellazione del post con ID: ${req.params.id}`);
+  });
+  
 // esportiamo il router per app js
 module.exports = router
